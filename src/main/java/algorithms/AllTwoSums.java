a74e8bcaadb053acb39d5158c94ff54b46019da3
Hanna Baronina <hanna.baronina@gmail.com>
2020-11-11 16:36:26 -0800

    Add AllTwoSums, TwoSums and ArrayListOperations algorithms.

---
+++ src/main/java/algorithms/AllTwoSums.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args){
+        int[] array = {3,5,2,4,6,7,2,6};
+        int target = 9;
+        Map<Integer, Integer> foundNumbers = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i <array.length; i++){
+            int secondNumber = target - array[i];
+            if (foundNumbers.containsKey(secondNumber)){
+                int[] result = {foundNumbers.get(secondNumber), i};
+                System.out.println(Arrays.toString(result));
+            } else {
+                foundNumbers.put(array[i], i);
+            }
+        }
+
+    }
+}
